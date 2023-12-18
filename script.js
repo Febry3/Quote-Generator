542c651b3ba90e6751cd944ce088e403042e98f4
Feeee <Fee@gmail.com>
2023-12-18 20:53:30 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const url = "https://api.quotable.io/random";
+
+async function getQuote (url) {
+    const response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+}
+
+getQuote(url);
